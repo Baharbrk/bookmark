07990acb6949d8045d0e6f74bb036ebb52d3cc42
bbarakati <b.barakati@ixolit.com>
2022-05-19 21:16:30 +0200

    Add Test case for validating URL for saveUrl Method

--- src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarkTest.java
+++ src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarkTest.java
@@ -2,15 +2,24 @@ package pt.ipp.isep.dei.examples.tdd.basic.domain;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BookmarkTest {
 
     @Test
     public void TestAddUrl() {
-        Bookmark.saveUrl result = new Bookmark.saveUrl("https://www.facebook.com/");
+        // Act
+        boolean result = new Bookmark.saveUrl("https://www.facebook.com/");
         // Assert
-        assertEquals(true, result);
+        assertTrue(result);
+    }
+
+    @Test
+    public void TestValidatedUrlWithInvalidUrl() {
+        // Act
+        boolean result = new Bookmark.validateUrl("httpsgoogle.com/");
+        // Assert
+        assertFalse(result);
     }
 }
 
