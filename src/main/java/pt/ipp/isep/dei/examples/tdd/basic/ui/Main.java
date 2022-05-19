ab91952ba277dd563340bbdba606ad6d09226305
bbarakati <b.barakati@ixolit.com>
2022-05-19 22:21:28 +0200

    Add main files

---
+++ src/main/java/pt/ipp/isep/dei/examples/tdd/basic/ui/Main.java
@@ -0,0 +1,10 @@
+package pt.ipp.isep.dei.examples.tdd.basic.ui;
+
+import pt.ipp.isep.dei.examples.tdd.basic.domain.Bookmark;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Bookmark bookmark = new Bookmark();
+    }
+}
