2ac853aa0b5e7aa26aba75515b7ef4144a0016b4
bbarakati <b.barakati@ixolit.com>
2022-05-23 22:15:38 +0200

    Main file

--- src/main/java/pt/ipp/isep/dei/examples/tdd/basic/ui/Main.java
+++ src/main/java/pt/ipp/isep/dei/examples/tdd/basic/ui/Main.java
@@ -1,10 +1,8 @@
 package pt.ipp.isep.dei.examples.tdd.basic.ui;
 
-import pt.ipp.isep.dei.examples.tdd.basic.domain.Bookmark;
-
 public class Main {
 
     public static void main(String[] args) {
-        Bookmark bookmark = new Bookmark();
+
     }
 }
--- src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarkTest.java
+++ src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarkTest.java
@@ -8,8 +8,12 @@ public class BookmarkTest {
 
     @Test
     public void TestAddWithValidUrl() {
+        // Arrange
+        Url url = new Url();
+        url.setUrl("https://www.facebook.com/");
+
         // Act
-        boolean result = new Bookmark().saveUrl("https://www.facebook.com/");
+        boolean result = new Bookmark().saveUrl(url.getUrl());
         // Assert
         assertTrue(result);
     }
@@ -19,30 +23,6 @@ public class BookmarkTest {
         // Act
         boolean result = new Bookmark().saveUrl("dhdhdebook.com/");
         // Assert
-        assertFalse(result);
-    }
-
-    @Test
-    public void TestTagWithValidUrl() {
-        // check if url exist, if not add , otherwise save tag
-        boolean result = new Bookmark().tagUrl("tag", "https://www.facebook.com");
-        assertTrue(result); // get list of tags from bookmark , check if the count is as same as expected ,
-        // we don't know if the tag  is really saved => we should check if it is saved
-        // bookmark , tag class, url class
-        // list of bookmarks => list of bookmark class
-    }
-
-    @Test
-    public void TestTagWithInvalidUrl() {
-        boolean result = new Bookmark().tagUrl("tag", "hbbt.facebook.com");
-        assertFalse(result);
-    }
-
-    @Test
-    public void testIsUrlExistedWithNewUrl() {
-        Bookmark bookmark = new Bookmark();
-        bookmark.saveUrl("https://www.facebook.com/");
-        boolean result = new Bookmark().doesUrlExist("https://www.facebook.com/");
         assertFalse(result);
     }
 }
