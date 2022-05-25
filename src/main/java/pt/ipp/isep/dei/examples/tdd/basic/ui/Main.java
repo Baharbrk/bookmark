70d7e09fdf556c54fb3a03d0768f2507069c81dd
bbarakati <b.barakati@ixolit.com>
2022-05-25 12:41:55 +0200

    Add bookmark functionality to Main class

--- src/main/java/pt/ipp/isep/dei/examples/tdd/basic/ui/Main.java
+++ src/main/java/pt/ipp/isep/dei/examples/tdd/basic/ui/Main.java
@@ -1,6 +1,7 @@
 package pt.ipp.isep.dei.examples.tdd.basic.ui;
 
 import pt.ipp.isep.dei.examples.tdd.basic.domain.Bookmark;
+import pt.ipp.isep.dei.examples.tdd.basic.domain.Url;
 
 import java.util.ArrayList;
 
@@ -10,5 +11,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+    }
+    public void bookmarkUrl(String url) {
+        Bookmark bookmark = bookmarks.stream().filter(obj -> obj.url.url == url).findFirst().orElse(null);
+        if (bookmark == null) {
+            Url urlObj = new Url(url);
+            this.bookmarks.add(new Bookmark(urlObj));
+        } else {
+            bookmark.addRate();
+        }
     }
 }
